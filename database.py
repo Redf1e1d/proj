662b49e903ed176fa6e83042e985d36a45678c3d
Redf1e1d <stoansergej99@gmail.com>
2026-03-01 20:29:44 +0200

    fix requirements

--- database.py
+++ database.py
@@ -2,9 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 import os
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+#DATABASE_URL = os.getenv("DATABASE_URL")
 
-#DATABASE_URL = "postgresql://postgres:gthtpfhz@localhost/dev_db"
+DATABASE_URL = "postgresql://postgres:gthtpfhz@localhost/dev_db"
 
 engine = create_engine(DATABASE_URL)
 
